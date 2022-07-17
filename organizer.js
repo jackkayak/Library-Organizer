db8ef7ed1421b58bf35c42842e0266ae7ec62cd4
jackkayak <jacktkay99@gmail.com>
2022-07-17 12:02:06 -0700

    first commit

---
+++ organizer.js
@@ -0,0 +1,59 @@
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+  
+    get title() {
+      return this._title;
+    }
+  
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+  
+    get ratings() {
+      return this._ratings;
+    }
+  
+    set isCheckedOut(value) {
+      this._isCheckedOut = value;
+    }
+  
+    toggleCheckOutStatus() {
+      this.isCheckedOut = !this.isCheckedOut;
+    }
+  
+    getAverageRating() {
+      let ratingsSum = 
+    this.ratings.reduce((accumulator, rating) =>
+    accumulator + rating);
+        return ratingsSum/ this.ratings.length;
+    }
+  
+  addRating(value) {
+    this.ratings.push(value);
+  }
+  };
+  
+  
+  class Book extends Media {
+    constructor(title, author, pages) {
+      super(title)
+      this._author = author;
+      this._pages = pages;
+    }
+  
+    get author() {
+      return this._author;
+  
+    }
+  
+    get pages() {
+      return this._pages;
+    }
+  }
+  
+  const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+  console.log(historyOfEverything)
